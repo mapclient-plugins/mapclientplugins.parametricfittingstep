65c78eedd0750f47b839365f8539fb636c89aa64
TimmyNic <tim.nicolas@outlook.com>
2021-06-11 12:52:11 +1200

    Updated resources_rc import.

--- mapclientplugins/parametricfittingstep/view/ui_parametricfittingwidget.py
+++ mapclientplugins/parametricfittingstep/view/ui_parametricfittingwidget.py
@@ -14,7 +14,7 @@ from PySide2.QtWidgets import *
 
 from opencmiss.utils.zinc.widgets.basesceneviewerwidget import BaseSceneviewerWidget
 
-from  . import resources_rc
+from .. import resources_rc
 
 class Ui_ParametricFittingWidget(object):
     def setupUi(self, ParametricFittingWidget):
