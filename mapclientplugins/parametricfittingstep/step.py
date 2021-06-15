a8b55036b24db4610a1a79e658e3e9530c8122f2
Hugh Sorby <h.sorby@auckland.ac.nz>
2021-06-16 08:03:13 +1200

    Update step.py

--- mapclientplugins/parametricfittingstep/step.py
+++ mapclientplugins/parametricfittingstep/step.py
@@ -178,7 +178,7 @@ class ParametricFittingStep(WorkflowStepMountPoint):
         """
         self._config.update(json.loads(string))
 
-        d = ConfigureDialog(self._main_window)
+        d = ConfigureDialog()
         d.identifierOccursCount = self._identifierOccursCount
         d.setConfig(self._config)
         self._configured = d.validate()
